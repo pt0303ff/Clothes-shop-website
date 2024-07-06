e2f9768a83f63494689e50fa2692d9881cdad0c2
Ponting_yu <85691121+pt0303ff@users.noreply.github.com>
2024-07-06 15:03:14 +0800

    Update product_data.php

--- product_data.php
+++ product_data.php
@@ -2,25 +2,25 @@
 
 $Item = array(
     // 商品名稱, 商品價錢, 商品說明, 圖片檔名, 性別
-    "1" => array("Man Jaket 1", "399", "This jacket is very nice!", "menjaket1.jpg", "man"),
-    "2" => array("Man Jaket 2", "1099", "This jacket is very nice!", "menjaket2.jpg", "man"),
-    "3" => array("Man Jaket 3", "499", "This jacket is very nice!", "menjaket3.jpg", "man"),
-    "4" => array("Man Cloth 1", "399", "This jacket is very nice!", "mencloth1.jpg", "man"),
-    "5" => array("Man Cloth 2", "1099", "This jacket is very nice!", "mencloth2.jpg", "man"),
-    "6" => array("Man Cloth 3", "499", "This jacket is very nice!", "mencloth3.jpg", "man"),
-    "7" => array("Man Bottom 1", "399", "This pants is very nice!", "menbottem1.jpg", "man"),
-    "8" => array("Man Bottom 2", "1099", "This pants is very nice!", "menbottem2.jpg", "man"),
-    "9" => array("Man Bottom 3", "499", "This pants is very nice!", "menbottem3.jpg", "man"),
+    "1" => array("Man Jaket 1", "399", "This jacket is very nice!", "img/menjaket1.jpg", "man"),
+    "2" => array("Man Jaket 2", "1099", "This jacket is very nice!", "img/menjaket2.jpg", "man"),
+    "3" => array("Man Jaket 3", "499", "This jacket is very nice!", "img/menjaket3.jpg", "man"),
+    "4" => array("Man Cloth 1", "399", "This jacket is very nice!", "img/mencloth1.jpg", "man"),
+    "5" => array("Man Cloth 2", "1099", "This jacket is very nice!", "img/mencloth2.jpg", "man"),
+    "6" => array("Man Cloth 3", "499", "This jacket is very nice!", "img/mencloth3.jpg", "man"),
+    "7" => array("Man Bottom 1", "399", "This pants is very nice!", "img/menbottem1.jpg", "man"),
+    "8" => array("Man Bottom 2", "1099", "This pants is very nice!", "img/menbottem2.jpg", "man"),
+    "9" => array("Man Bottom 3", "499", "This pants is very nice!", "img/menbottem3.jpg", "man"),
     
-    "10" => array("Woman Jaket 1", "399", "This jacket is very nice!", "womanjacket1.jpg", "woman"),
-    "11" => array("Woman Jaket 2", "1099", "This jacket is very nice!", "womanjacket2.jpg", "woman"),
-    "12" => array("Woman Jaket 3", "499", "This jacket is very nice!", "womanjacket3.jpg", "woman"),
-    "13" => array("Woman Cloth 1", "399", "This cloth is very nice!", "womencloth1.jpg", "woman"),
-    "14" => array("Woman Cloth 2", "1099", "This cloth is very nice!", "womencloth2.jpg", "woman"),
-    "15" => array("Woman Cloth 3", "499", "This cloth is very nice!", "womencloth3.jpg", "woman"),
-    "16" => array("Woman Bottom 1", "399", "This bottom is very nice!", "womenbottem1.jpg", "woman"),
-    "17" => array("Woman Bottom 2", "1099", "This bottom is very nice!", "womenbottem2.jpg", "woman"),
-    "18" => array("Woman Bottom 3", "499", "This bottom is very nice!", "womenbottem3.jpg", "woman")
+    "10" => array("Woman Jaket 1", "399", "This jacket is very nice!", "img/womanjacket1.jpg", "woman"),
+    "11" => array("Woman Jaket 2", "1099", "This jacket is very nice!", "img/womanjacket2.jpg", "woman"),
+    "12" => array("Woman Jaket 3", "499", "This jacket is very nice!", "img/womanjacket3.jpg", "woman"),
+    "13" => array("Woman Cloth 1", "399", "This cloth is very nice!", "img/womencloth1.jpg", "woman"),
+    "14" => array("Woman Cloth 2", "1099", "This cloth is very nice!", "img/womencloth2.jpg", "woman"),
+    "15" => array("Woman Cloth 3", "499", "This cloth is very nice!", "img/womencloth3.jpg", "woman"),
+    "16" => array("Woman Bottom 1", "399", "This bottom is very nice!", "img/womenbottem1.jpg", "woman"),
+    "17" => array("Woman Bottom 2", "1099", "This bottom is very nice!", "img/womenbottem2.jpg", "woman"),
+    "18" => array("Woman Bottom 3", "499", "This bottom is very nice!", "img/womenbottem3.jpg", "woman")
 
 );
 
