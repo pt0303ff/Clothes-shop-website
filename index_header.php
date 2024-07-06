cde00e9b7e79037dbe207fcd835510999b3db05d
Ponting_yu <85691121+pt0303ff@users.noreply.github.com>
2024-07-06 14:57:20 +0800

    Update index_header.php

--- index_header.php
+++ index_header.php
@@ -8,7 +8,7 @@
           <li><a href="Aboutus.php">About us</a></li>
           <li><a href="Contact.php">Contact</a></li>
           <li><a href="OnlineService.php">Online Service</a></li>
-          <li style="float: right;" id="left"><a href="index.php"><img src="home.png" width="25px"></a></li>
-          <li style="float: right;" id="left"><a href="cart.php"><img src="cart.png" width="30px" height="30px"></a></li>
+          <li style="float: right;" id="left"><a href="index.php"><img src="img/home.png" width="25px"></a></li>
+          <li style="float: right;" id="left"><a href="cart.php"><img src="img/cart.png" width="30px" height="30px"></a></li>
       </ul>
     </nav>
